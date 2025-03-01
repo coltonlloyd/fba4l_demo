0f690737e2283d72276627100b9714ec2d7bd472
Colton Lloyd <coltonlloyd@MacBookPro.attlocal.net>
2025-03-01 13:36:08 -0800

    update

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
 
-setup(name="fba4l_course_material",
+setup(name="fba4l_demo",
       version="",
       author="Colton Lloyd",
-      url="https://github.com/coltonlloyd/fba4l_course_material",
+      url="https://github.com/coltonlloyd/fba4l_demo",
       packages=find_packages(),
       install_requires=['cobra']
                        
